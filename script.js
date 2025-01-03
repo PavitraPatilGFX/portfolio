a2ccd020fc5cf516ed260e5f72bf6ee122a27af4
PavitraPatilGFX <kgyt2023@gmail.com>
2025-01-03 21:16:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ const images = [
 // Render each image
 images.forEach(({ src, alt }) => {
     const imageCard = document.createElement("div");
-    imageCard.classList.add("relative", "group", "rounded-corners");
+    imageCard.classList.add("relative", "group");
+
+    // Image container with aspect ratio
     imageCard.innerHTML = `
-        <img src="${src}" alt="${alt}" class="w-full rounded-lg shadow-md">
+        <div class="relative w-full max-w-sm aspect-w-4 aspect-h-3 overflow-hidden rounded-lg shadow-md">
+            <img src="${src}" alt="${alt}" class="w-full h-full object-cover">
+        </div>
         <div class="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 flex justify-center items-center transition-opacity">
             <p class="text-white text-lg font-semibold">${alt}</p>
         </div>
